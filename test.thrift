5759f3081e23a097bff54c16444742fdf42268b0
Charles Lavery <charles.lavery@gmail.com>
2014-03-30 19:53:01 -0400

    add thrift definition

---
+++ test.thrift
@@ -0,0 +1,33 @@
+namespace js Test.Something
+
+exception MessageExistsException {
+  /**
+   * The error message
+   */
+  1: string message;
+}
+
+enum Operation {
+  ADD = 1,
+  SUBTRACT = 2,
+  MULTIPLY = 3,
+  DIVIDE = 4
+}
+
+const i32 INT32CONSTANT = 9853
+
+typedef bool OperationTwo
+
+struct Message {
+  1: string text;
+  2: string date;
+}
+
+service BulletinBoard {
+  /**
+   * Adds a new message.
+   */
+  void add(1: Message msg) throws (1:MessageExistsException messageExistsException);
+  list<Message> get();
+}
+
